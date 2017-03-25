d22989665ec336791085a404fe911987f06f515a
phoenix403 <aryan.multani93@gmail.com>
2017-03-25 18:37:08 +0100

    updated intialize_db file

--- includes/config/DB_config.php
+++ includes/config/DB_config.php
@@ -9,6 +9,9 @@ defined("DB_DB") ? "":define("DB_DB","the-techy-juan");
 
 //__TABLE CONFIG__
 defined("TABLE_CONTACT_US") ? "":define("TABLE_CONTACT_US","contact_us");
+defined("TABLE_PRODUCTS") ? "":define("TABLE_PRODUCTS","products");
+defined("TABLE_PRODUCTS_DESCRIPTION") ? "":define("TABLE_PRODUCTS_DESCRIPTION","products_description");
+defined("TABLE_MANUFACTURERS") ? "":define("TABLE_MANUFACTURERS"," manufacturers");
 
 
 //other server configs..
--- includes/setup/intialize_db.php
+++ includes/setup/intialize_db.php
@@ -5,9 +5,15 @@
 include "../config/DB_config.php";
 include "../func.php";
 
-if(!is_table_exists($DB_CONN, "contact_us")) {
+
+/*
+
+	Contact us table
+
+*/
+if(!is_table_exists($DB_CONN, TABLE_CONTACT_US)) {
 	
-     $cntct_tble_crt_qry = "CREATE TABLE IF NOT EXISTS contact_us(
+     $cntct_tble_crt_qry = "CREATE TABLE IF NOT EXISTS ".TABLE_CONTACT_US."(
 				id_contact_us	integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
 				contact_name	varchar(255) NOT NULL,
 				contact_mail	varchar(255) NOT NULL,
@@ -15,10 +21,75 @@ if(!is_table_exists($DB_CONN, "contact_us")) {
 				date_created  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 			)";
 	echo $DB_CONN->exec($cntct_tble_crt_qry) ? 
-		"<br/>Table <b>contact_us</b> Failed!<br>" : 
-		"<br/>Table <b>contact_us</b> created successfully!<br>";
+		"<br/>Table <b>".TABLE_CONTACT_US."</b> Failed!<br>" : 
+		"<br/>Table <b>".TABLE_CONTACT_US."</b> created successfully!<br>";
 }
 
+
+
+/*
+
+	Table Products
+
+*/
+if(!is_table_exists($DB_CONN, TABLE_PRODUCTS)) {
+	
+     $prdct_tble_crt_qry = "CREATE TABLE IF NOT EXISTS ".TABLE_PRODUCTS."(
+			id_products			integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+			EAN					varchar(255) NOT NULL,
+			product_model 		varchar(255) NOT NULL,
+			manufacturers_id	integer NOT NULL,
+			product_weight		decimal 	NOT NULL,
+			product_quantity	integer 	NOT NULL,
+			product_image		varchar(255) NOT NULL,
+			product_status		tinyint(1) 	NOT NULL,
+			product_price		decimal NOT NULL,
+			product_date_added  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			product_date_modified  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+			)";
+	echo $DB_CONN->exec($prdct_tble_crt_qry) ? 
+		"<br/>Table <b>".TABLE_PRODUCTS."</b> Failed!<br>" : 
+		"<br/>Table <b>".TABLE_PRODUCTS."</b> created successfully!<br>";
+}
+
+
+/*
+
+	Products Description table where you can hold description in different languages
+
+*/
+if(!is_table_exists($DB_CONN, TABLE_PRODUCTS_DESCRIPTION)) {
+	
+     $pd_tble_crt_qry = "CREATE TABLE IF NOT EXISTS ".TABLE_PRODUCTS_DESCRIPTION."(
+			id_products			integer NOT NULL,
+			language_id			integer NOT NULL,
+			products_name 		varchar(255) NOT NULL,
+			products_description text NOT NULL,
+			products_viewed 	integer NOT NULL
+			)";
+	echo $DB_CONN->exec($pd_tble_crt_qry) ? 
+		"<br/>Table <b>".TABLE_PRODUCTS_DESCRIPTION."</b> Failed!<br>" : 
+		"<br/>Table <b>".TABLE_PRODUCTS_DESCRIPTION."</b> created successfully!<br>";
+}
+
+/*
+
+	manufaction table
+
+*/
+if(!is_table_exists($DB_CONN, TABLE_MANUFACTURERS)) {
+	
+     $mnfctr_tble_crt_qry = "CREATE TABLE IF NOT EXISTS ".TABLE_MANUFACTURERS."(
+			id_manufacturers	integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+			manufacturer_name	integer NOT NULL,
+			manufacturer_image	integer NOT NULL,
+			date_created  	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			date_last_modified  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+			)";
+	echo $DB_CONN->exec($mnfctr_tble_crt_qry) ? 
+		"<br/>Table <b>".TABLE_MANUFACTURERS."</b> Failed!<br>" : 
+		"<br/>Table <b>".TABLE_MANUFACTURERS."</b> created successfully!<br>";
+}
 
 
 echo "<br/> Script Finished !<br/>";
