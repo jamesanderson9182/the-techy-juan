330f0f0aec1b108716f346ff4cb8717470e8d246
Chris Starr <christian@starrdev.us>
2017-03-25 16:01:52 -0400

    Added desktop navigation bar.

--- includes/nav.php
+++ includes/nav.php
@@ -33,6 +33,30 @@
     </div>
 
     <div class="desktop-navigation">
-
+        <div class="center">
+            <div class="desktop-navigation-cart">
+                <a href="cart.php">Shopping Cart(0)</a>
+                <a style="color:grey;">&nbsp;|&nbsp;</a>
+                <a href="#">Login</a>
+                <a style="color:grey;">&nbsp;|&nbsp;</a>
+                <a href="#">Sign-up</a>
+            </div>
+        </div>
+        <div class="desktop-navigation-container">
+            <div class="desktop-navigation-logo"></div>
+            <div class="desktop-navigation-right">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="forum.php">Forum</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+                <div class="desktop-navigation-search">
+                    <span class="desktop-navigation-search-icon"><i class="fa fa-search"></i></span>
+                    <input type="search" id="search" placeholder="Search..." />
+                </div>
+            </div>
+        </div>
     </div>
 </div>
