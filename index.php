585cca0c765189ae3ea42b8a5abcd9ec67d286c3
Chris Starr <christian@starrdev.us>
2017-03-26 14:34:52 -0400

    Updated index.php layout

--- index.php
+++ index.php
@@ -2,25 +2,46 @@
 include "includes/head.php";
 include "models/product.php";
 ?>
+	<div class="center-content">
     <div class="youtube">
-        <div class="youtube-center">
-        	<iframe src="https://www.youtube.com/embed/pQ9L3HUHEFs" frameborder="0" allowfullscreen>
-			</iframe>
-        </div>
-        <div class="youtube-tile yt-tile-1"
-             onClick="window.location='https://www.youtube.com/watch?v=ggD-B3BzBwQ'"></div>
-        <div class="youtube-tile yt-tile-2"
-             onClick="window.location='https://www.youtube.com/watch?v=zWNo8kZ27bU'"></div>
-        <div class="youtube-tile yt-tile-3"
-             onClick="window.location='https://www.youtube.com/watch?v=2nL-KXxXgRQ'"></div>
+        <div class="youtube-top">
+	        <div class="youtube-center">
+	        	<iframe src="https://www.youtube.com/embed/pQ9L3HUHEFs" frameborder="0" allowfullscreen>
+				</iframe>
+	        </div>
+	        <div class="youtube-title">
+					<a>Welcome to Techy Juan</a>
+					<p>gjdfklgjfdkljgfdlkjglkfdjsglkfjgkjglkjdfglkjfdlkgjfdlkgjfdlkjgkldfs</p>
+				</div>
+	    </div>
+	    <div class="youtube-responsive-container">
+	        <div class="youtube-responsive">
+		        <div class="youtube-tile yt-tile-1"
+		             onClick="window.location='https://www.youtube.com/watch?v=ggD-B3BzBwQ'"></div><!--
+		        --><div class="youtube-tile yt-tile-2"
+		             onClick="window.location='https://www.youtube.com/watch?v=zWNo8kZ27bU'"></div><!--
+		        --><div class="youtube-tile yt-tile-3"
+		             onClick="window.location='https://www.youtube.com/watch?v=2nL-KXxXgRQ'"></div>
+		    </div><!--
+	        --><div class="youtube-responsive">
+		        <div class="youtube-tile yt-tile-2"
+		             onClick="window.location='https://www.youtube.com/watch?v=2nL-KXxXgRQ'"></div><!--
+		        --><div class="youtube-tile yt-tile-1"
+		             onClick="window.location='https://www.youtube.com/watch?v=2nL-KXxXgRQ'"></div><!--
+		        --><div class="youtube-tile yt-tile-3"
+		             onClick="window.location='https://www.youtube.com/watch?v=2nL-KXxXgRQ'"></div>
+	        </div>
+	    </div>
+
         <div class="youtube-view" onClick="window.location='https://www.youtube.com/channel/UCzFnpBkmgBF3IHdX_adsOww'">
             <a>Visit Channel</a>
         </div>
     </div>
     <div class="divider"></div>
     <div class="project">
+    	
         <a class="project-title">Looking to start a project?</a>
-
+		<div class="center-text">
         <div class="project-tile project1" onClick="window.location='./projects/home-automation.php'">
             <div class="project-filter"></div>
             <a>Home Automation</a>
@@ -39,7 +60,9 @@ include "models/product.php";
         </div>
         <a href="#" class="project-viewmore">View more&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i></a>
     </div>
+    </div>
     <div class="divider"></div>
+    </div>
 <?php
 $db = mysqli_connect('localhost', 'root', '', 'products')
 or die('Error connecting to MySQL server.');
@@ -63,7 +86,9 @@ or die('Error connecting to MySQL server.');
 	            }
 	            ?>
 	        </div>
-        	<a class="products-view">View More&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+	        <div class="center-text">
+        		<a class="f-products-view" href="#">View More&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+        	</div>
     </div>
 <?php
 include 'includes/foot.php';
