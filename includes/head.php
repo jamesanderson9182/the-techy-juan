8fd429a91cdcaa28fbeadcf7e2bb8f6aff2e375d
James Anderson <jamesanderson9182@users.noreply.github.com>
2017-03-26 17:18:27 +0100

    update file path of css and js. This will break local site if not using virtual hosts. Ask James for help if you are stuck

--- includes/head.php
+++ includes/head.php
@@ -6,12 +6,12 @@
     <meta name="description" content=" ... ">
     <meta name="author" content="Starr Development">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/master.css?v=1.0">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/func.js"></script>
-    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/master.css?v=1.0">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/func.js"></script>
+    <script src="/js/main.js"></script>
 </head>
 <?php
 include 'nav.php'
