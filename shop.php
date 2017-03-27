c59bc6f87313525053887cd4a2e86cbc675d88c4
Chris Starr <christian@starrdev.us>
2017-03-27 04:03:35 -0400

    Made master css class for content on pages(.page & .page-inner) Updated the Shop.php with new layout concept

--- shop.php
+++ shop.php
@@ -1,8 +1,26 @@
 <?php
 include "includes/head.php";
+include "models/product.php";
 ?>
-    <div class="down-for-maintenance">
-        <a>Our Shop is currently under maintenance!</a>
+    <div class="page">
+        <div class="page-inner"><a>This is our shop</a></div>
+        <div class="page-inner">
+            <?php 
+                $products = Product::All();
+                foreach ($products as $product){
+            ?>
+                <div class="home-featured-product">
+                    <img src="http://placehold.it/300x300">
+                    <h1><?= $product->product_name ?></h1>
+                    <p><?= $product->getStars() ?></p>
+                    <p><?= $product->product_price ?></p>
+                    <a class="home-featured-product-btn" href="#">View Product</a>
+                </div><!--
+                -->
+            <?php
+                }
+            ?>
+        </div>
     </div>
 <?php
 include 'includes/foot.php';
