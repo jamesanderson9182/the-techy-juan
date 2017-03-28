1f1cf1d5a8852c5d6d9030d3bb37e70d3d30b76c
James Anderson <jamesanderson9182@users.noreply.github.com>
2017-03-28 19:46:16 +0100

    updating shop to work with new Product table

--- shop.php
+++ shop.php
@@ -5,15 +5,16 @@ include "models/product.php";
     <div class="page">
         <div class="page-inner"><a>This is our shop</a></div>
         <div class="page-inner">
-            <?php 
+            <?php
                 $products = Product::All();
-                foreach ($products as $product){
+                /** @var Product $product */
+            foreach ($products as $product){
             ?>
                 <div class="home-featured-product">
                     <img src="http://placehold.it/300x300">
-                    <h1><?= $product->product_name ?></h1>
+                    <h1><?= $product->Name ?></h1>
                     <p><?= $product->getStars() ?></p>
-                    <p><?= $product->product_price ?></p>
+                    <p><?= $product->getPrice() ?></p>
                     <a class="home-featured-product-btn" href="#">View Product</a>
                 </div><!--
                 -->
