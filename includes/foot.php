edb9181e18da3137bf68ae758ddf1ccfb710f353
Chris Starr <christian@starrdev.us>
2017-03-29 05:43:45 -0400

    Re-designed social media display in 'foot.php'.

--- includes/foot.php
+++ includes/foot.php
@@ -1,11 +1,14 @@
 
 <div class="page">
     <div class="page-inner">
-        <div class="social">
-            <a href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-google fa-2x" aria-hidden="true"></i></a>
+        <div id="social">
+            <div id="social-left">
+            <h1>"Some edgy quote said here"<a>-Mr. Edge</a></h1>
+            </div>
+            <a class="social-icon" href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
+            <a class="social-icon" href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+            <a class="social-icon" href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+            <a class="social-icon" href="#"><i class="fa fa-google fa-2x" aria-hidden="true"></i></a>
         </div>
     </div>
 </div>
