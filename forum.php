d4342c21187465b38b17c84ba8a4ce0735dd9098
Chris Starr <christian@starrdev.us>
2017-04-01 09:22:43 -0400

    Updated title for 'forum.php'

--- forum.php
+++ forum.php
@@ -2,7 +2,9 @@
 include "includes/head.php";
 ?>
     <div class="page">
-        <div class="page-inner"><a>Forum is under maintenance</a></div>
+        <div class="page-inner" style="text-align: left;">
+			<a class="page-title">The Techy Juan Forum</a>
+		</div>
         <div class="page-inner">
         	<div style="height:400px;"></div>
         </div>
